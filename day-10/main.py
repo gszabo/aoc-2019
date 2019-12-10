aa7620810fba5dcd18c7e207c9b9ef2a335b3c00
Gabor Szabo <harry.bme@gmail.com>
2019-12-10 19:41:55 +0100

    day 10 part 2

--- day-10/main.py
+++ day-10/main.py
@@ -1,5 +1,9 @@
+import math
+from collections import defaultdict
 from dataclasses import dataclass
-from pprint import pprint
+
+# x goes right
+# y goes down
 
 
 @dataclass
@@ -11,7 +15,7 @@ class Point:
         return hash((self.x, self.y))
 
     def __sub__(self, other):
-        return DirectionVector(other.x - self.x, other.y - self.y)
+        return DirectionVector(self.x - other.x, self.y - other.y)
 
 
 @dataclass
@@ -47,10 +51,10 @@ def gcd(a: int, b: int) -> int:
     return a
 
 
-def part_one():
+def read_asteroids(path):
     asteroids = []
 
-    with open("./input.txt") as f:
+    with open(path) as f:
         lines = list(map(list, map(str.strip, f.readlines())))
 
     for y in range(0, len(lines)):
@@ -58,20 +62,106 @@ def part_one():
             if lines[y][x] == "#":
                 asteroids.append(Point(x, y))
 
-    def visible_asteroids(start):
-        directions = set()
+    return asteroids
 
-        for asteroid in asteroids:
-            if asteroid == start:
-                continue
 
-            directions.add(asteroid - start)
+def visible_count_from(viewpoint, all):
+    directions = set()
 
-        return len(directions)
+    for asteroid in all:
+        if asteroid == viewpoint:
+            continue
 
-    answer = max(map(visible_asteroids, asteroids))
+        directions.add(asteroid - viewpoint)
 
-    pprint(answer)
+    return viewpoint, len(directions)
+
+
+def angle_with_upwards(vector: DirectionVector) -> float:
+    """
+    Returns the rotation angle (in radians) 
+    from the upward unit vector to the given vector.
+    """
+
+    upward = DirectionVector(0, -1)
+    downward = DirectionVector(0, 1)
+
+    if vector == upward:
+        return 0
+    elif vector == downward:
+        return math.pi
+    elif vector.x > 0:
+        return math.pi / 2 + math.atan(vector.y / vector.x)
+    elif vector.x < 0:
+        return 3 * math.pi / 2 + math.atan(vector.y / vector.x)
+
+
+def distance_from(p0):
+    sqr = lambda x: x * x
+
+    def distance(p):
+        return sqr(p0.x - p.x) + sqr(p0.y - p.y)
+
+    return distance
+
+
+def collect_by_direction(asteroids, start_point):
+    asteroids_by_direction = defaultdict(list)
+
+    for asteroid in asteroids:
+        direction = asteroid - start_point
+        asteroids_by_direction[direction].append(asteroid)
+
+    for asteroids_in_direction in asteroids_by_direction.values():
+        asteroids_in_direction.sort(key=distance_from(start_point))
+
+    return asteroids_by_direction
+
+
+def vaporize(asteroids_by_direction):
+    asteroid_count = sum(map(len, asteroids_by_direction.values()))
+
+    directions = list(asteroids_by_direction.keys())
+    directions.sort(key=angle_with_upwards)
+
+    vaporized = []
+
+    while len(vaporized) < asteroid_count:
+        for direction in directions:
+            asteroids_in_direction = asteroids_by_direction[direction]
+            if len(asteroids_in_direction) > 0:
+                vaporized.append(asteroids_in_direction.pop(0))
+
+    return vaporized
+
+
+def part_one():
+    asteroids = read_asteroids("./input.txt")
+    answer = max(
+        map(lambda ast: visible_count_from(viewpoint=ast, all=asteroids), asteroids),
+        key=lambda pair: pair[1],
+    )
+    print(answer)
+
+
+def part_two():
+    # result from part one
+    start_point = Point(14, 17)
+
+    asteroids = read_asteroids("./input.txt")
+    asteroids.remove(start_point)
+
+    asteroids_by_direction = collect_by_direction(asteroids, start_point)
+
+    vaporized = vaporize(asteroids_by_direction)
+
+    luckiest = vaporized[199]
+    answer = 100 * luckiest.x + luckiest.y
+
+    print(answer)
+
 
 if __name__ == "__main__":
     part_one()
+    print()
+    part_two()

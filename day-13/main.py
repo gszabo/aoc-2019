eaf3e2a999210a6443443638823c3601e7d87ac4
Gabor Szabo <harry.bme@gmail.com>
2019-12-14 22:58:44 +0100

    day 13 part 2 hacky solution

--- day-13/main.py
+++ day-13/main.py
@@ -1,6 +1,10 @@
 from __future__ import annotations
 
 from collections import defaultdict
+from curses import wrapper
+from time import sleep
+
+import curses
 
 from computer import Computer, InputReader
 
@@ -12,21 +16,84 @@ BALL = 4
 
 CHARS = {EMPTY: " ", WALL: "#", BLOCK: "X", HORIZONTAL_PADDLE: "=", BALL: "o"}
 
+BALL_POS = {"x": 0}
+PADDLE_POS = {"x": 0}
+
 
 class Screen:
     def __init__(self):
         self._grid = defaultdict(int)
+        self.score = 0
+        self.max_score = 0
+        
 
     def put_tile(self, x, y, tile_type):
         self._grid[(x, y)] = tile_type
+        if tile_type == BALL:
+            BALL_POS["x"] = x
+        if tile_type == HORIZONTAL_PADDLE:
+            PADDLE_POS["x"] = x
 
     def number_of_tiles(self, tile_type):
         return len([tile for tile in self._grid.values() if tile == tile_type])
 
+    def print_to(self, stdscr):
+        stdscr.clear()
+
+        for y in range(0, 25):
+            for x in range(0, 40):
+                stdscr.addstr(y, x, CHARS[self._grid[(x, y)]])
+
+        stdscr.addstr(11, 50, "Score:")
+        stdscr.addstr(12, 50, str(self.score))
+
+        stdscr.addstr(11, 70, "Max score:")
+        stdscr.addstr(12, 70, str(self.max_score))
+
+        stdscr.refresh()
+
+
+class JoystickInput:
+    def __init__(self, stdscr):
+        self._stdscr = stdscr
+        self._value = 0
+
+    def has_next(self):
+        return True
+
+    def read_next(self):
+        try:
+            c = self._stdscr.getch()
+            if c == curses.KEY_LEFT:
+                self.left()
+            elif c == curses.KEY_RIGHT:
+                self.right()
+            elif c == curses.KEY_DOWN:
+                self._value = 0
+        except:
+            pass
+        # sleep(0.5)
+        # result = self._value
+        # self._value = 0
+        # return result
+        if PADDLE_POS["x"] < BALL_POS["x"]:
+            return 1
+        elif BALL_POS["x"] < PADDLE_POS["x"]:
+            return -1
+        else:
+            return 0
+
+    def left(self):
+        self._value = -1
+
+    def right(self):
+        self._value = 1
+
 
 class ScreenOutputWriter:
-    def __init__(self, screen: Screen):
+    def __init__(self, screen: Screen, stdscr):
         self._screen = screen
+        self._stdscr = stdscr
         self._buffer = []
 
     def write(self, value):
@@ -35,8 +102,18 @@ class ScreenOutputWriter:
             self._flush_to_screen()
 
     def _flush_to_screen(self):
-        x, y, tile_type = self._buffer
-        self._screen.put_tile(x, y, tile_type)
+        x, y, value = self._buffer
+        
+        if x == -1 and y == 0:
+            # TODO: refactor this
+            self._screen.max_score = max(value, self._screen.score)
+            self._screen.score = value
+        else:
+            self._screen.put_tile(x, y, value)
+
+        if self._stdscr is not None:
+            self._screen.print_to(self._stdscr)
+        
         self._buffer.clear()
 
 
@@ -46,7 +123,7 @@ def part_one():
 
     screen = Screen()
 
-    c = Computer(program, InputReader([]), ScreenOutputWriter(screen))
+    c = Computer(program, InputReader([]), ScreenOutputWriter(screen, None))
     c.run()
 
     answer = screen.number_of_tiles(BLOCK)
@@ -54,7 +131,7 @@ def part_one():
     print(answer)
 
 
-def part_two():
+def part_two(stdscr):
     # screen is about 40x25
     # maybe try the curses library
     # https://docs.python.org/3/howto/curses.html#curses-howto
@@ -63,13 +140,22 @@ def part_two():
         program = list(map(int, f.readline().strip().split(",")))
 
     screen = Screen()
-    # TODO: segment display for score
-    # TODO: joystick input
+    joystick = JoystickInput(stdscr)
     # TODO: maybe throttle computer running?
 
-    c = Computer(program, InputReader([]), ScreenOutputWriter(screen))
+    curses.halfdelay(10)
+
+    # insert coin
+    program[0] = 2
+
+    c = Computer(program, joystick, ScreenOutputWriter(screen, stdscr))
     c.run()
+
+    curses.nocbreak()
+
+    stdscr.getkey()
+
 
 
 if __name__ == "__main__":
-    part_one()
+    wrapper(part_two)

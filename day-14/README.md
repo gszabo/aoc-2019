ac31838c5eadce81ffcdf4032971de3409ac6804
Gabor Szabo <harry.bme@gmail.com>
2019-12-14 16:49:27 +0100

    day 14 cleanup, readme

---
+++ day-14/README.md
@@ -0,0 +1,9 @@
+# Day 14
+
+https://adventofcode.com/2019/day/14
+
+The model that worked for me at the end is similar to a wallet with different currencies,
+and the chemical equations are strange, one-way exchange methods for changing currency.
+
+I allow the wallet to contain negative amount of a currency (meaning in "debt"), so I can
+work my way backwards from FUEL to ORE. See more explanation in the comments.
--- day-14/main.py
+++ day-14/main.py
@@ -1,106 +1,62 @@
 from collections import defaultdict
-import math
 
-with open("./input.txt") as f:
-    lines = list(map(str.strip, f.readlines()))
 
-reactions = {}
-ore_stuff = {}
+def read_equations(path):
+    chemical_equations = {}
 
-for line in lines:
-    resources, result = line.split(" => ")
-    
-    amount, element = result.split(" ")
-    amount = int(amount)
-    
-    rs = {}
+    with open(path) as f:
+        lines = list(map(str.strip, f.readlines()))
 
-    for resource in resources.split(", "):
-        am, el = resource.split(" ")
-        am = int(am)
-        rs[el] = am
+    for line in lines:
+        source_side, result_side = line.split(" => ")
 
-    reactions[element] = {
-        "amount": amount,
-        "resources": rs,
-    }
+        result_amount, result_element = result_side.split(" ")
+        result_amount = int(result_amount)
 
-    if "ORE" in rs:
-        ore_stuff[element] = {
-            "ore_amount": rs["ORE"],
-            "result_amount": amount,
+        source_chemicals = {}
+
+        for source_item in source_side.split(", "):
+            amount, chemical = source_item.split(" ")
+            amount = int(amount)
+            source_chemicals[chemical] = amount
+
+        chemical_equations[result_element] = {
+            "result_amount": result_amount,
+            "input": source_chemicals,
         }
 
-def lookup_in_first_order_resource(target_resource, target_amount):
-    result = defaultdict(int)
-    
-    r = reactions[target_resource]
-    
-    multiplier = target_amount // r["amount"]
-    if (target_amount % r["amount"]) > 0:
-        multiplier += 1
-    
-    for resource, amount in r["resources"].items():
-        if resource in ore_stuff:
-            result[resource] += amount * multiplier
-        else:
-            xxx = lookup_in_first_order_resource(resource, amount * multiplier)
-            for vmi_resource, vmi_amount in xxx.items():
-                result[vmi_resource] += vmi_amount
-
-    return result
-
-def lookup_in_ore(target_resource, target_amount):
-    xx = ore_stuff[target_resource]
-    ore_amount = xx["ore_amount"]
-    result_amount = xx["result_amount"]
-    multiplier = target_amount // result_amount
-    if (target_amount % result_amount) > 0:
-        multiplier += 1
-    return ore_amount * multiplier
-
-def print_graph():
-    for result, vmi in reactions.items():
-        for resource in vmi["resources"].keys():
-            print(f'"{resource}" -> "{result}"')
+    return chemical_equations
 
 
-from pprint import pprint
-
-# pprint(reactions)
-# pprint(ore_stuff)
-
-# fuel_in_first_order = lookup_in_first_order_resource("FUEL", 1) 
-
-# pprint(fuel_in_first_order)
-
-# sum_ore = 0
-# for resource, amount in fuel_in_first_order.items():
-#     sum_ore += lookup_in_ore(resource, amount)
-
-# print(sum_ore)
-
-# visualize in http://www.webgraphviz.com/
-# print_graph()
-
+chemical_equations = read_equations("./input.txt")
 
 
 def add_to_lab(lab, chemical, amount):
-    produce_info = reactions[chemical]
-    
-    multiplier = amount // produce_info["amount"]
-    if (amount % produce_info["amount"]) > 0:
-        multiplier += 1
-    
-    for chem, am in produce_info["resources"].items():
-        lab[chem] -= am*multiplier
-    
-    lab[chemical] += produce_info["amount"]*multiplier
+    """
+    Updates the laboratory resource info by applying the
+    equation corresponding to the given chemical creation.
+    If there is not enough input chemicals for the eqution,
+    those will go below zero, appearing as "debt".
+    """
 
-def part_one():
-    lab = defaultdict(int)
-    add_to_lab(lab, "FUEL", 1)
+    produce_info = chemical_equations[chemical]
+    amount_per_reaction = produce_info["result_amount"]
 
+    reaction_count = amount // amount_per_reaction
+    if (amount % amount_per_reaction) > 0:
+        reaction_count += 1
+
+    for input_chemical, input_amount in produce_info["input"].items():
+        lab[input_chemical] -= input_amount * reaction_count
+
+    lab[chemical] += amount_per_reaction * reaction_count
+
+
+def create_missing_chemicals(lab):
+    """
+    Until there is resource "debt" in the lab, it creates the missing
+    resources, by spending ORE.
+    """
     while True:
         negative_keys = list(filter(lambda k: k != "ORE" and lab[k] < 0, lab.keys()))
         if len(negative_keys) == 0:
@@ -108,41 +64,49 @@ def part_one():
         chemical_to_add = negative_keys[0]
         add_to_lab(lab, chemical_to_add, abs(lab[chemical_to_add]))
 
-    pprint(lab)
-    print(-lab["ORE"])
+
+def part_one():
+    # Here we create an empty lab, and add one FUEL to it.
+    # After paying the debt in ORE, the ORE deficit is
+    # going to tell the price of a FUEL.
+    lab = defaultdict(int)
+
+    add_to_lab(lab, "FUEL", 1)
+    create_missing_chemicals(lab)
+
+    answer = -lab["ORE"]
+
+    print(answer)
 
 
 def part_two():
-    lab = defaultdict(int)
-
     ORE_AMOUNT = 1000000000000
+    ORE_PER_FUEL = 346961  # From part 1
 
-    ORE_PER_FUEL = 346961
-
+    lab = defaultdict(int)
     lab["ORE"] = ORE_AMOUNT
 
     while lab["ORE"] > ORE_PER_FUEL:
+        # We know we can create at least
+        # lab["ORE"] // ORE_PER_FUEL amount of fuel.
+        # We need to go in these big steps, otherwise
+        # the calculation is too slow.
         add_to_lab(lab, "FUEL", lab["ORE"] // ORE_PER_FUEL)
-        while True:
-            negative_keys = list(filter(lambda k: k != "ORE" and lab[k] < 0, lab.keys()))
-            if len(negative_keys) == 0:
-                break
-            chemical_to_add = negative_keys[0]
-            add_to_lab(lab, chemical_to_add, abs(lab[chemical_to_add]))
+        create_missing_chemicals(lab)
 
     while lab["ORE"] >= 0:
+        # There might be enough leftover resource for more fuel.
+        # If ORE counts goes below zero, we know we tried to
+        # create one too many fuel
         add_to_lab(lab, "FUEL", 1)
-        while True:
-            negative_keys = list(filter(lambda k: k != "ORE" and lab[k] < 0, lab.keys()))
-            if len(negative_keys) == 0:
-                break
-            chemical_to_add = negative_keys[0]
-            add_to_lab(lab, chemical_to_add, abs(lab[chemical_to_add]))
-    
-    pprint(lab)
-    print(lab["FUEL"])
-    print(lab["FUEL"]-1)
+        create_missing_chemicals(lab)
+
+    answer = lab["FUEL"] - 1
+
+    print(answer)
 
 
 if __name__ == "__main__":
+    part_one()
+    print()
     part_two()

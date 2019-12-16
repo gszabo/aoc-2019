d3777211c61861ea96ab92ba8cef37951772771f
Gabor Szabo <harry.bme@gmail.com>
2019-12-16 19:55:10 +0100

    day 16 part 1

---
+++ day-16/main.py
@@ -0,0 +1,57 @@
+def pattern(digit_pos):
+    base_pattern = [0, 1, 0, -1]
+    i = 0
+    while True:
+        for _ in range(0, digit_pos):
+            yield base_pattern[i]
+        i = (i + 1) % len(base_pattern)
+
+
+def take(n, gen):
+    i = 0
+    for item in gen:
+        if i < n:
+            yield item
+        else:
+            return
+        
+        i += 1
+
+def skip(n, gen):
+    i = 0
+    for item in gen:
+        if n <= i:
+            yield item
+        
+        i += 1
+
+def algo_phase(digits):
+    result = []
+    for i in range(0, len(digits)):
+        digit_pos = i+1
+        s = 0
+        for digit, pat_digit in zip(digits, skip(1, pattern(digit_pos))):
+            s += digit * pat_digit
+        result.append(abs(s) % 10)
+
+    return result
+
+def text_to_digits(text):
+    return [int(ch) for ch in text]
+
+def digits_to_text(digits):
+    return "".join(map(str, digits))
+
+
+def part_one():
+    with open("./input.txt") as f:
+        input = f.readline().strip()
+    digits = text_to_digits(input)
+    for _ in range(0, 100):
+        digits = algo_phase(digits)
+    answer = digits_to_text(digits)[:8]
+    print(answer)
+
+
+if __name__ == "__main__":
+    part_one()

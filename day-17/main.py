7eb43698b007561ebf97061fc379a334de8d4b1b
Gabor Szabo <harry.bme@gmail.com>
2019-12-26 21:53:01 +0100

    day 17 part 2

--- day-17/main.py
+++ day-17/main.py
@@ -16,14 +16,14 @@ def part_one():
     with open("./input.txt") as f:
         program = list(map(int, f.readline().strip().split(",")))
 
-    buffer = []
-    output = OutputWriter(buffer)
+    output_buffer = []
+    output_writer = OutputWriter(output_buffer)
 
-    c = Computer(program, InputReader([]), output)
+    c = Computer(program, InputReader([]), output_writer)
 
     c.run()
 
-    text = "".join(map(chr, buffer)).strip()
+    text = "".join(map(chr, output_buffer)).strip()
     lines = text.split("\n")
 
     answer = 0
@@ -36,6 +36,37 @@ def part_one():
     print(answer)
 
 
+def part_two():
+    with open("./input.txt") as f:
+        program = list(map(int, f.readline().strip().split(",")))
+
+    # wake up robot
+    program[0] = 2
+
+    input_text = (
+        "A,B,A,C,B,C,B,C,A,C\n"
+        "R,12,L,10,R,12\n"
+        "L,8,R,10,R,6\n"
+        "R,12,L,10,R,10,L,8\n"
+        "n\n"
+    )
+    input_buffer = list(map(ord, input_text))
+    input_reader = InputReader(input_buffer)
+
+    output_buffer = []
+    output_writer = OutputWriter(output_buffer)
+
+    c = Computer(program, input_reader, output_writer)
+
+    c.run()
+
+    answer = output_buffer[-1]
+
+    print(answer)
+
+
 if __name__ == "__main__":
-    part_one()
+    # part_one()
+    # print()
+    part_two()
 
